11c868048d7a4f3c6b52765f4ffcc91cf6fb6f6a
maximkudryavtsev <maxim.kudryavtsev@omega-r.com>
2019-04-22 08:26:54 +0300

    Change clientId for Google Auth

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,7 +8,7 @@ export class Login extends Component {
     render(): ReactNode {
         return (
             <GoogleLogin
-                clientId={"432192894002-qbr08npl8o15ugp1m535o4f21j7qrv1i.apps.googleusercontent.com"}
+                clientId={"432192894002-m86cb5spa87uvrhhfnnu8h71gdqr7svd.apps.googleusercontent.com"}
                 buttonText={"Войти"}
                 onSuccess={(response) => console.log(response)}
                 onFailure={(error) => console.log(error)}
